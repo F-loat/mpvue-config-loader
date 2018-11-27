19e5f84e57ac3a4f4a689753884d66581b45d91b
F-loat <chaimaoyuan@foxmail.com>
2018-11-27 17:01:56 +0800

    feat: 功能初步完成

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # mpvue-config-loader
+
+## 安装
+
+``` bash
+npm i mpvue-config-loader -D
+```
+
+## 使用
+
+``` js
+// webpack.base.conf.js
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: [
+          {
+            loader: 'mpvue-loader',
+            options: vueLoaderConfig
+          },
+          'mpvue-config-loader'
+        ]
+      },
+      ...
+    ]
+  }
+  ...
+}
+```
+
+``` vue
+<script>
+// src/xx/xxx.vue
+export default {
+  config: {
+    navigationBarTitleText: '页面标题'
+  }
+}
+</script>
+```
---
+++ index.js
@@ -0,0 +1,61 @@
+const parser = require('@babel/parser')
+const traverse = require('@babel/traverse').default
+const generate = require('@babel/generator').default
+const t = require('@babel/types')
+
+function traverseObjectNode(node) {
+  if (node.type === 'ObjectProperty') {
+    const { properties } = node.value
+
+    return properties.reduce((result, prop) => {
+      result[prop.key.name] = traverseObjectNode(prop.value)
+      return result
+    }, {})
+  }
+
+  if (node.type === 'ObjectExpression') {
+    const { properties } = node
+
+    return properties.reduce((result, prop) => {
+      const key = t.isIdentifier(prop.key) ? prop.key.name : prop.key.value
+      result[key] = traverseObjectNode(prop.value)
+      return result
+    }, {})
+  }
+
+  if (node.type === 'ArrayExpression') {
+    return node.elements.map(item => traverseObjectNode(item))
+  }
+
+  if (node.type === 'NullLiteral') {
+    return null
+  }
+
+  return node.value
+}
+
+module.exports = function (source) {
+  const { resourcePath, emitFile } = this
+  const fileName = resourcePath.replace(/^.*src\\/, '').replace(/\.vue$/, '')
+
+  return source.replace(/<script>([^]*)<\/script>/, (match, $1) => {
+    const ast = parser.parse($1, { sourceType: 'module' });
+
+    traverse(ast, {
+      ObjectProperty(astPath) {
+        const { node, parentPath } = astPath
+        const { container } = parentPath
+
+        if (container.type === 'ExportDefaultDeclaration' && node.key.name === 'config') {
+          configObj = traverseObjectNode(node)
+          emitFile(`${fileName}.json`, JSON.stringify(configObj, null, 2))
+          astPath.remove()
+        }
+      }
+    })
+
+    const { code } = generate(ast)
+
+    return `<script>${code}<\/script>`
+  })
+}
